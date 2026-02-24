19c199d56e658b81848e6f261683d0f5c9df188a
James Tlaloc <jamestlaloc.dev@gmail.com>
2026-02-24 15:13:46 -0800

    Add new div element with content class and text.

--- js/script.js
+++ js/script.js
@@ -1,2 +1,5 @@
 const container = document.querySelector("#container");
 
+const content = document.createElement("div");
+content.classList.add("content");
+content.textContent = "This is the glorius test-content!";
