41715220612af1ff35f573a50c8c5f8fac1779f4
James Tlaloc <jamestlaloc.dev@gmail.com>
2026-02-24 15:15:43 -0800

    Add button.

--- js/script.js
+++ js/script.js
@@ -2,4 +2,6 @@ const container = document.querySelector("#container");
 
 const content = document.createElement("div");
 content.classList.add("content");
-content.textContent = "This is the glorius test-content!";
+content.textContent = "This is the glorius text-content!";
+
+container.appendChild(content);
